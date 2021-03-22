cd0aa009f6b0c9448e55effbfa04c2d291efb684
Gustaf Räntilä <g.rantila@gmail.com>
2021-03-23 00:06:14 +0100

    fix(errors): made sure extend errors are named and inherited properly

--- lib/errors.ts
+++ lib/errors.ts
@@ -1,11 +1,20 @@
 
 export class DuplicateError extends Error
-{ }
+{
+	constructor( message?: string )
+	{
+		super( message );
+		Object.setPrototypeOf( this, DuplicateError.prototype );
+		this.name = this.constructor.name;
+	}
+}
 
 export class DuplicateConstraintError extends DuplicateError
 {
 	constructor( field: string )
 	{
 		super( `Constraint ${field} already set.` );
+		Object.setPrototypeOf( this, DuplicateConstraintError.prototype );
+		this.name = this.constructor.name;
 	}
 }
